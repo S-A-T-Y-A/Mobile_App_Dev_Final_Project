0b57c80e8ceea8dae9c6d2ddc079a3b553f19cd7
S-A-T-Y-A <satyanandan846@gmail.com>
2024-04-13 02:40:11 -0400

    logo is created and flash page is added with logo

--- app/src/main/java/com/team3/skill_comrade/MainActivity.kt
+++ app/src/main/java/com/team3/skill_comrade/MainActivity.kt
@@ -3,12 +3,15 @@ package com.team3.skill_comrade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.team3.skill_comrade.ui.theme.SkillComradeTheme
 
@@ -22,25 +25,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    LogoImage()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SkillComradeTheme {
-        Greeting("Android")
-    }
+fun LogoImage(){
+    val SC_logo= painterResource(R.drawable.logo)
+    Image(painter = SC_logo, contentDescription = null, contentScale = ContentScale.Crop)
 }
